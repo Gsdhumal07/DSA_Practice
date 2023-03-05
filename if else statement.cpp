49219eee3b5ecaa35eb7a15079a6c62abb267730
Ganesh Dhumal <89744859+Gsdhumal07@users.noreply.github.com>
2023-03-06 00:22:53 +0530

    Add files via upload

---
+++ if else statement.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"welcome ";
+	int no;
+	cout<<"Plese enter your no."<<endl;
+	cin>>no;
+//	if (no>5){
+//		cout<<"Please go through second way"<<endl;
+//		
+//	}
+//	else if (no==5){
+//		cout<<"plse go straight";
+//		
+//	}
+//	else{
+//		cout<<" go through first way";
+//	}
+//
+//	
+//	switch(no){
+//		case 5:
+//			cout<<"your are fail"<<endl;
+//			break ;
+//		case 7:
+//			cout<<"your are on boundry"<<endl;
+//			break ;	
+//		case 10:
+//			cout<<"your are pass "<<endl;
+//			break ;	
+//		default	:
+//			cout<<"other all are absent for the exam "<<endl;
+//			break ;
+//	}	
+//	
+	return 0;
+
+}
